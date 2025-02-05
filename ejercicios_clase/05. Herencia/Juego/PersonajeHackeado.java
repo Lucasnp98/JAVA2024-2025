1d9d204c0286f38e6533ae55866f358a2c995678
Lucasnp98 <108325074+Lucasnp98@users.noreply.github.com>
2025-02-05 10:24:38 +0100

    Add files via upload

---
+++ ejercicios_clase/05. Herencia/Juego/PersonajeHackeado.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+class PersonajeHackeado extends Personaje
+{
+    String trucos;
+    PersonajeHackeado(String nombre, Arma arma, String trucos)
+    {
+        super(nombre, arma);
+        this.trucos = trucos;
+
+    }
+
+    
+
+    void addTruco(String truco)
+    {
+        this.trucos += ";" + truco;
+    }
+    
+
+
+
+    String getInfo()
+    {
+        return super.getInfo() + " Trucos: " + trucos;
+    }
+
+
+    boolean contiene_truco(String truco)
+    {
+        String[] trucos_split = trucos.split(";");
+        return Arrays.asList(trucos_split).contains(truco);
+    }
+    @Override
+void disparar(Personaje p) {
+    boolean aimbotActivo = contiene_truco("AIMBOT");
+
+    boolean acierta = aimbotActivo || Math.random() < arma.getProbabilidad();
+
+    if (acierta) {
+        if (contiene_truco("MAXIMODAÑO")) {
+            p.herir(VIDA_MAX); // Mata instantáneamente
+        } else {
+            p.herir(arma.getDanio());
+        }
+    }
+    if (!contiene_truco("MUNICIONINFINITA")) {
+        arma.restar_balas();
+    }
+}
+
+    
+
+}
